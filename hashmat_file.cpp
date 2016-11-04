3d88852d38253a5b61e6531c02312052f51877f3
James Obrador <pogipohako123@gmail.com>
2016-11-05 07:26:26 +0800

    Add files via upload

---
+++ hashmat_file.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	ifstream f("input.txt");
+	int hashmat, opponent;
+	while(f>>hashmat>>opponent)
+	{
+		cout<<abs(hashmat-opponent)<<endl;
+	}
+	system("pause");
+	return 0;
+}
